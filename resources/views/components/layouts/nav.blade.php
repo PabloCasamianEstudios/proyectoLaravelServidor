ad5179d4bd47240e460cdd7725fe9a8535cd4d65
pablocasamianestudios <pablocasamianestudios@gmail.com>
2025-02-07 13:15:32 +0100

    layout del nav

---
+++ resources/views/components/layouts/nav.blade.php
@@ -0,0 +1,5 @@
+<div class="navbar bg-base-100 flex content-center justify-around">
+    <a class="btn btn-ghost text-xl">Boton1</a>
+    <a class="btn btn-ghost text-xl">Boton2</a>
+    <a class="btn btn-ghost text-xl">Boton3</a>
+</div>
