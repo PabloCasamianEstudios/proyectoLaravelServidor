b7326be07c56582ced247d3fdfe325a57f3bf836
pablocasamianestudios <pablocasamianestudios@gmail.com>
2025-02-11 12:07:32 +0100

    header para movil y pc

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,8 +1,10 @@
-<header class="header md:h-10v bg-header flex flex-row md:flex-row   justify-around items-center p-3">
+<header class="header md:h-15v bg-header
+flex flex-row md:flex-row justify-around items-center p-3">
 
-    <img class="w-20 max-h-full p-1" src="{{asset ("images/logo.png")}}" alt="logo">
-    <h1 class="text-white-900 text-7xl">CLUB SECRETO</h1>
-    <div>
+    <img class="w-20 max-h-full p-1 md:max-h-full" src="{{asset ("images/logo.png")}}" alt="logo">
+    <h1 class="hidden md:block text-white-900 text-7xl">CLUB SECRETO</h1>
+    {{--Header pc--}}
+    <div class="hidden md:flex flex-row space-x-3">
         @auth
             {{auth()->user()->name}}
             <form action="{{route("logout")}}" method="post">
@@ -20,4 +22,30 @@
 
         @endguest
     </div>
+
+    {{--Header movil--}}
+    <div class="block md:hidden">
+        <input type="checkbox" name="" id="menu-toggle" class=" peer hidden">
+        <label  class="text-2xl hover:cursor-pointer text-white" for="menu-toggle">
+            &#9778;
+        </label>
+
+
+        <div class="absolute hidden peer-checked:block bg-gray-300 p-3 rounded-xl flex flex-col">
+        @auth
+            {{auth()->user()->name}}
+            <form action="{{route("logout")}}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-error">Logout</button>
+            </form>
+        @endauth
+        @guest
+            <div class="flex flex-row gap-5">
+                <a class="btn btn-glass" href="login">Login</a>
+                <a class="btn btn-glass" href="register">Register</a>
+            </div>
+        @endguest
+        </div>
+    </div>
+
 </header>
